f1252527f46dba0ec376077c9541b7e9b6ca3120
Max Ulanov <ulanovmaks29@gmail.com>
2024-04-04 11:53:39 +0300

    Add axios default instance

---
+++ src/api/axios.api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axios;
