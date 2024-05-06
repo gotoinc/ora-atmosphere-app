593b501ab32f6cea1167bd850fd9afd25ddf0368
Max Ulanov <ulanovmaks29@gmail.com>
2024-05-06 14:00:12 +0300

    Update sidebar and main search

--- src/components/search/MainSearch.vue
+++ src/components/search/MainSearch.vue
@@ -1,10 +1,10 @@
 <template>
-    <form
+    <div
         v-click-outside="() => (isFiltersOpen = false)"
         :class="{
             active: isFiltersOpen,
         }"
-        class="search bordered relative w-full transition-all max-tab:static max-tab:ml-auto max-tab:!max-w-24"
+        class="search bordered relative w-full transition-all max-tab:static max-tab:ml-auto max-tab:w-fit max-tab:!border-none"
     >
         <form class="search__wrap">
             <!-- Search input -->
@@ -13,90 +13,115 @@
                     v-model="searchValue"
                     type="text"
                     class="w-full bg-transparent"
-                    @focus="isFiltersOpen = true"
+                    @focus="toggleSidebar"
                 />
 
-                <component :is="IconSearch" class="h-6 w-6 flex-shrink-0" />
+                <component
+                    :is="IconSearch"
+                    class="h-6 w-6 flex-shrink-0 max-tab:hidden"
+                />
+
+                <component
+                    :is="IconClose"
+                    class="block h-4 w-4 flex-shrink-0 cursor-pointer fill-current tab:hidden"
+                    @click="isFiltersOpen = false"
+                />
             </div>
 
             <!-- Search filters -->
             <div class="search__filters">
-                <!-- Recent searches list -->
-                <div class="mb-8">
-                    <h3 class="mb-2 text-xs font-bold opacity-70">
-                        Recent searches
-                    </h3>
+                <div class="overflow -mr-5 overflow-y-auto pr-5">
+                    <!-- Recent searches list -->
+                    <div class="mb-8">
+                        <h3 class="mb-2 text-xs font-bold opacity-70">
+                            Recent searches
+                        </h3>
 
-                    <ul>
-                        <li
-                            v-for="i in 3"
-                            :key="i"
-                            class="flex cursor-pointer items-center gap-3 rounded p-1 text-xs font-bold transition-colors hover:bg-white-25"
-                        >
-                            <component
-                                :is="IconSearch"
-                                class="h-4 w-4 flex-shrink-0 opacity-70"
-                            />
+                        <ul>
+                            <li
+                                v-for="i in 3"
+                                :key="i"
+                                class="flex cursor-pointer items-center gap-3 rounded p-1 text-xs font-bold transition-colors hover:bg-white-25"
+                            >
+                                <component
+                                    :is="IconSearch"
+                                    class="h-4 w-4 flex-shrink-0 opacity-70"
+                                />
 
-                            Search
-                        </li>
-                    </ul>
-                </div>
+                                Search
+                            </li>
+                        </ul>
+                    </div>
 
-                <!-- Categories options -->
-                <div class="mb-6 text-sm font-bold">
-                    <h3 class="mb-3.5">Categories</h3>
+                    <!-- Categories options -->
+                    <div class="mb-6 text-sm font-bold">
+                        <h3 class="mb-3.5">Categories</h3>
 
-                    <ul class="grid grid-cols-3 gap-3">
-                        <li
-                            v-for="category in categories"
-                            :key="category.id"
-                            :class="{
-                                '!opacity-100':
-                                    category.id === selectedCategory?.id,
-                            }"
-                            class="flex cursor-pointer items-center gap-2.5 opacity-70 transition-opacity"
-                            @click="selectedCategory = category"
-                        >
-                            <div
-                                class="h-14 w-14 flex-shrink-0 rounded-lg bg-white-75"
-                            ></div>
+                        <ul class="grid grid-cols-3 gap-3 max-mob:grid-cols-2">
+                            <li
+                                v-for="category in categories"
+                                :key="category.id"
+                                :class="{
+                                    '!opacity-100':
+                                        category.id === selectedCategory?.id,
+                                }"
+                                class="flex cursor-pointer items-center gap-2.5 opacity-70 transition-opacity"
+                                @click="selectedCategory = category"
+                            >
+                                <div
+                                    class="h-14 w-14 flex-shrink-0 rounded-lg bg-white-75"
+                                ></div>
 
-                            <h4 class="text-xs font-semibold">
-                                {{ category.name }}
-                            </h4>
-                        </li>
-                    </ul>
-                </div>
+                                <h4 class="text-xs font-semibold">
+                                    {{ category.name }}
+                                </h4>
+                            </li>
+                        </ul>
+                    </div>
 
-                <!-- Tags options -->
-                <div>
-                    <h3 class="mb-3.5">Topics/Tags</h3>
+                    <!-- Tags options -->
+                    <div>
+                        <h3 class="mb-3.5">Topics/Tags</h3>
 
-                    <ul class="flex flex-wrap gap-2">
-                        <li
-                            v-for="i in 9"
-                            :key="i"
-                            class="bordered cursor-pointer px-4 py-1 text-xs font-semibold lowercase transition-colors hover:bg-white-100 hover:text-dark"
-                        >
-                            schools
-                        </li>
-                    </ul>
+                        <ul class="flex flex-wrap gap-2">
+                            <li
+                                v-for="i in 9"
+                                :key="i"
+                                class="bordered cursor-pointer px-4 py-1 text-xs font-semibold lowercase transition-colors hover:bg-white-100 hover:text-dark"
+                            >
+                                schools
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </form>
 
-        <button class="hidden">
+        <!-- Button for tablet and mobile -->
+        <button
+            class="search__btn"
+            :class="{
+                'bg-white-15': isFiltersOpen,
+            }"
+            @click="toggleSidebar"
+        >
             <component :is="IconSearch" class="h-6 w-6 flex-shrink-0" />
         </button>
-    </form>
+    </div>
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue';
+    import IconClose from '@img/icons/close.svg?component';
     import IconSearch from '@img/icons/search.svg?component';
 
     import type { Category } from '@/ts/interfaces/category';
+
+    interface Emits {
+        (e: 'opened'): void;
+    }
+
+    const emits = defineEmits<Emits>();
 
     import searchCategories from '@/json/search-categories.json';
 
@@ -106,33 +131,62 @@
     const categories = ref<Category[]>([...searchCategories]);
 
     const selectedCategory = ref<null | Category>(null);
+
+    const toggleSidebar = () => {
+        if (isFiltersOpen.value) {
+            isFiltersOpen.value = false;
+        } else {
+            isFiltersOpen.value = true;
+            emits('opened');
+        }
+    };
 </script>
 
 <style scoped>
     .search {
         max-width: 514px;
 
+        .overflow {
+            max-height: calc(var(--screen) - 145px);
+
+            &::-webkit-scrollbar {
+                @apply w-1.5;
+            }
+
+            &::-webkit-scrollbar-thumb {
+                @apply rounded-md bg-white-100;
+            }
+        }
+
         &__input {
             @apply relative z-10 flex items-center gap-2 px-3.5 py-1.5 transition-all;
 
             input {
-                @apply -mb-2 border-b border-solid border-transparent pb-2 transition-all;
+                @apply -mb-2 border-b border-solid border-transparent pb-2 transition-all max-tab:mb-0;
             }
         }
 
         &__wrap {
-            @apply transition-all max-tab:invisible max-tab:absolute max-tab:left-1/2 max-tab:top-full max-tab:w-4/5 max-tab:-translate-x-1/2 max-tab:opacity-0;
+            @apply transition-all max-tab:invisible max-tab:absolute max-tab:left-1/2 max-tab:top-full max-tab:w-full max-tab:-translate-x-1/2 max-tab:opacity-0;
         }
 
         &__filters {
-            @apply bordered absolute -top-1.5 left-0 w-full bg-gradient-search pt-14 opacity-0 transition-all;
+            @apply bordered absolute -top-1.5 left-0 w-full pt-14 opacity-0 transition-all max-tab:top-0 max-tab:h-vh max-tab:rounded-none max-tab:border-none max-tab:bg-primary tab:bg-gradient-search;
+        }
+
+        &__btn {
+            @apply flex h-8 w-8 items-center justify-center rounded-md transition-colors tab:hidden;
         }
 
         &.active {
             @apply border-transparent;
 
+            .search__wrap {
+                @apply visible opacity-100;
+            }
+
             .search__input {
-                @apply px-9;
+                @apply px-9 max-tab:gap-6 max-tab:pr-6;
 
                 input {
                     @apply border-white-100;
